0b2ea5a4dd8e6ec1f51f5b8675e6562be036beb0
Andrey Ruban <anruban2001@gmail.com>
2023-08-15 18:46:52 +0300

    change repository name + hw2

--- hw2/Program.cs
+++ hw2/Program.cs
@@ -1,10 +1,102 @@
-﻿namespace hw2
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+
+public enum Priority
 {
-    internal class Program
+    LOW,
+    NORMAL,
+    HIGH
+}
+
+public class CustomThreadPool
+{
+    private int threadCount;
+    private BlockingCollection<Tuple<Priority, Action<object>, object>> taskQueue;
+    private CancellationTokenSource cancellationTokenSource;
+    private Task[] workerThreads;
+
+    public CustomThreadPool(int threadCount)
     {
-        static void Main(string[] args)
+        this.threadCount = threadCount;
+        taskQueue = new BlockingCollection<Tuple<Priority, Action<object>, object>>();
+        cancellationTokenSource = new CancellationTokenSource();
+        workerThreads = new Task[threadCount];
+
+        for (int i = 0; i < threadCount; i++)
         {
-            Console.WriteLine("Hello, World!");
+            workerThreads[i] = Task.Run(() => Worker(cancellationTokenSource.Token));
         }
+    }
+
+    public bool Execute(Action<object> action, Priority priority, object value = null)
+    {
+        var task = new Tuple<Priority, Action<object>, object>(priority, action, value);
+        return taskQueue.TryAdd(task);
+    }
+
+    public void Stop()
+    {
+        cancellationTokenSource.Cancel();
+    }
+
+    private void Worker(CancellationToken cancellationToken)
+    {
+        foreach (var task in taskQueue.GetConsumingEnumerable(cancellationToken))
+        {
+            if (cancellationToken.IsCancellationRequested)
+                break;
+
+            if (task.Item1 == Priority.LOW)
+            {
+                foreach (var higherPriorityTask in taskQueue)
+                {
+                    if (higherPriorityTask.Item1 == Priority.HIGH)
+                    {
+                        return;
+                    }
+                }
+            }
+            else if (task.Item1 == Priority.NORMAL)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    if (taskQueue.TryTake(out var highPriorityTask))
+                    {
+                        highPriorityTask.Item2(highPriorityTask.Item3);
+                    }
+                }
+            }
+
+            task.Item2(task.Item3);
+        }
+    }
+}
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        CustomThreadPool threadPool = new CustomThreadPool(threadCount: 3);
+
+        threadPool.Execute(TaskMethod, Priority.LOW, "Low priority task 1");
+        threadPool.Execute(TaskMethod, Priority.NORMAL, "Normal priority task 1");
+        threadPool.Execute(TaskMethod, Priority.HIGH, "High priority task 1");
+        threadPool.Execute(TaskMethod, Priority.LOW, "Low priority task 2");
+        threadPool.Execute(TaskMethod, Priority.NORMAL, "Normal priority task 2");
+        threadPool.Execute(TaskMethod, Priority.HIGH, "High priority task 2");
+
+        Thread.Sleep(5000);
+
+        threadPool.Stop();
+        Console.WriteLine("Thread pool stopped.");
+
+        Console.ReadLine();
+    }
+
+    static void TaskMethod(object value)
+    {
+        Console.WriteLine($"Executing task: {value}");
     }
 }
--- hw3/App.xaml.cs
+++ hw3/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace nuget.org
+namespace hw3
 {
     /// <summary>
     /// Interaction logic for App.xaml
